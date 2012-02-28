b2da18ec54191e3c6b69bc4465e9e440a4b74b2b
Mike K <kelleher.mike@gmail.com>
2012-02-27 20:46:00 -0500

    fixed: c[j] coeff, yoursine

--- ChebSine.h
+++ ChebSine.h
@@ -7,54 +7,65 @@
 #include <cstdio>
 #include <fstream>
 
-#define ORDER 10
 // In c++, structs are much like classes, except they default to public
 // class LinearSine : public SinePrototype would also work here   
 struct ChebSine : SinePrototype
 {
 	private:
-		vector<double> xx;
-		vector<double> sineX;
 		int Npoints;
 		int Nintervals;
-		double co[ORDER];
-
+		vector<double> c;
+		vector<double> f;
+		double A;
+		double B;
+		double CI;
 	public:
 	// Define your functions here 
 	// Class Constructor
 	ChebSine(const int nn) : SinePrototype()  
 	{
 		// Initialize and prepare interpolation of sine
+		double yi;
+		bool debug = false;
+
 		Nintervals = nn;
 		Npoints = nn + 1;
-		xx.resize(Npoints);
-		sineX.resize(Npoints);
 
-		// Get coefficients from file
-      ifstream inFile("chebcoeff.txt");
-      int n = 0;
-		double coeffI;
-		while (inFile >> coeffI && n < ORDER) {
-			co[n++] = coeffI;
-			//cout << co[n-1]<<"\n";
+		c.resize(nn);
+		f.resize(nn);
+
+		A=0;
+		B=2*M_PI;
+		double bma=0.5*(B-A),bpa=0.5*(B+A);
+		for(int k=0;k<nn;k++){
+			yi=cos(M_PI*(k+0.5)/nn);	// Use zeros of nn-th Chebyshev polynomial as interp nodes
+			f[k]=sin(M_PI*(yi)-M_PI);	// Find sin(interpolation nodes)
 		}
-		inFile.close();
-		double A = 0;
-		double B = 2*M_PI;
-		
-		for(int i = 0; i < Npoints; i++) {
-			//xx[i] = i*2.*M_PI/Nintervals;
-			xx[i] = cos(M_PI*(i-.5)/Npoints)*((B-A)/2)+(B+A)/2;
-			sineX[i] = sin(xx[i]); 
+		for(int j = 0; j < nn ; j++) {
+			CI=0.0;
+			for( int k=0; k < nn; k++){
+				CI+=f[k]*cos(M_PI*j*(k+0.5)/nn);	// Determine coefficients
+			}
+			c[j]=CI*(2.0/nn);
+			if(debug){
+				cout << "I=" << j << " C=" << c[j] << "\n";
+			}
 		}
 	}
 
-	// Your sine function
 	double sine(double x) {
-		int j = (int)(x / (2. * M_PI) * Nintervals);
-		// This could probably be faster if we remove the if statement
-		if (j>=Nintervals) j = Nintervals-1;
-			return sineX[j] + ((x-xx[j])/(xx[j+1]-xx[j]))*(sineX[j+1]-sineX[j]);
+		double xi=(x-M_PI)/(M_PI);		//Adjust x to [-1,1]
+		double yi=0.0;						//sin(x) estimate
+		double d1=0.0,d2=0.0,sv=0.0;
+		// Use Clenshaw recursion to find f(x)=sum_n=0^N-1 (c_n*T_n)
+		for(int j=Nintervals-1;j>0;j--){
+			sv=d1;
+			d1=2.0*xi*d1-d2+c[j];
+			d2=sv;
 		}
-	};
+		yi=xi*d1-d2+0.5*c[0];			//Final step is different than all others
+		return yi;
+
+	}
+};
 #endif
